5b366b047d30b929063b8e4042e030b52413d3fc
Vishal Kumar <vishal.raj1234.vr@gmail.com>
2021-05-14 14:35:58 +0530

    intership project completed

---
+++ js/tata3.js
@@ -0,0 +1,10 @@
+function myFunction() {
+	// body...
+	if(document.getElementById("nav").style.display=="none")
+	{
+		document.getElementById("nav").style.display="inline-block";
+	}
+	else{
+		document.getElementById("nav").style.display="none";
+	}
+}
